44c727b0e310191428259b267fb1cd70ff9cea2e
Davi5266 <davi.souza09526@gmail.com>
2024-10-11 16:10:11 -0300

    ok!

--- 01/dist/js/index.js
+++ 01/dist/js/index.js
@@ -36,3 +36,105 @@ let a = 0;
 a = "teste";
 a = true;
 a = [];
+// union type
+let id = "10";
+id = 200;
+const userId = 10;
+const productId = "00001";
+const shirId = 123;
+// enum
+// tamanho de roupas (size: Médio, size: Pequeno)
+var Size;
+(function (Size) {
+    Size["P"] = "Pequeno";
+    Size["M"] = "M\u00E9dio";
+    Size["G"] = "Grande";
+})(Size || (Size = {}));
+const camisa = {
+    name: "Camisa gola V",
+    size: Size.G,
+};
+console.log(camisa);
+// literal types
+let teste;
+// teste = "outrovalor"
+teste = "autenticado";
+teste = null;
+// funções
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(12, 12));
+function sayHelloTo(name) {
+    return `Hello ${name}`;
+}
+console.log(sayHelloTo("Vanessa"));
+function logger(msg) {
+    console.log(msg);
+}
+logger("Teste!");
+function greeting(name, greet) {
+    if (greet) {
+        console.log(`Olá ${greet} ${name}`);
+    }
+    else {
+        console.log(`Olá ${name}`);
+    }
+}
+greeting("José");
+greeting("Pedro", "Sir");
+function sumNumbers(nums) {
+    return nums.n1 + nums.n2;
+}
+console.log(sumNumbers({ n1: 1, n2: 2 }));
+function multiplyNumbers(nums) {
+    return nums.n1 * nums.n2;
+}
+const someNumbers = {
+    n1: 5,
+    n2: 10,
+};
+console.log(multiplyNumbers(someNumbers));
+// narrowing
+// checagem de tipos
+function doSomething(info) {
+    if (typeof info === "number") {
+        console.log(`O número é ${info}`);
+        return;
+    }
+    console.log("Não foi passado um número!!!");
+}
+doSomething(5);
+doSomething(true);
+// generics
+function showArraysItems(arr) {
+    arr.forEach((item) => {
+        console.log(`ITEM: ${item}`);
+    });
+}
+const a1 = [1, 2, 3];
+const a2 = ["a", "b", "c"];
+showArraysItems(a1);
+showArraysItems(a2);
+// classes
+class User {
+    constructor(name, role, isApproved) {
+        this.name = name;
+        this.role = role;
+        this.isApproved = isApproved;
+    }
+    showUserName() {
+        console.log(`O nome do usuário é ${this.name}`);
+    }
+    showUserRole(canShow) {
+        if (canShow) {
+            console.log(`Idade do usuário é: ${this.role}`);
+            return;
+        }
+        console.log("Informação restrita!");
+    }
+}
+const zeca = new User("Zéca", "Admin", true);
+console.log(zeca);
+zeca.showUserName();
+zeca.showUserRole(false);
--- 01/src/index.ts
+++ 01/src/index.ts
@@ -83,8 +83,120 @@ const camisa = {
 console.log(camisa);
 
 // literal types
-let teste: "autenticado" | null
+let teste: "autenticado" | null;
 
 // teste = "outrovalor"
 teste = "autenticado";
 teste = null;
+
+// funções
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+console.log(sum(12,12));
+
+function sayHelloTo(name: string): string {
+    return `Hello ${name}`;
+}
+
+console.log(sayHelloTo("Vanessa"));
+
+function logger(msg: string): void { // Função não possui retorno
+    console.log(msg);
+}
+
+logger("Teste!");
+
+function greeting(name: string, greet?: string) {
+    if(greet) {
+        console.log(`Olá ${greet} ${name}`);
+    } else {
+        console.log(`Olá ${name}`)
+    }
+}
+
+greeting("José")
+greeting("Pedro", "Sir")
+
+// interfaces
+interface MathFunctionParams {
+    n1: number;
+    n2: number;
+}
+
+function sumNumbers(nums: MathFunctionParams) {
+    return nums.n1 + nums.n2;
+}
+
+console.log(sumNumbers({n1: 1, n2: 2}));
+
+function multiplyNumbers(nums: MathFunctionParams) {
+    return nums.n1 * nums.n2
+}
+
+const someNumbers: MathFunctionParams = {
+    n1: 5,
+    n2: 10,
+};
+
+console.log(multiplyNumbers(someNumbers));
+
+// narrowing
+// checagem de tipos
+function doSomething(info: number | boolean) {
+    if (typeof info === "number") {
+        console.log(`O número é ${info}`);
+        return;
+    }
+    console.log("Não foi passado um número!!!")
+}
+
+doSomething(5);
+doSomething(true);
+
+// generics
+function showArraysItems<T>(arr: T[]) {
+    arr.forEach((item) => {
+        console.log(`ITEM: ${item}`);
+    });
+}
+
+const a1 = [1, 2, 3];
+const a2 = ["a", "b", "c"];
+
+showArraysItems(a1);
+showArraysItems(a2);
+
+// classes
+class User {
+    name
+    role
+    isApproved
+
+    constructor(name: string, role: string, isApproved: boolean) {
+        this.name = name;
+        this.role = role;
+        this.isApproved = isApproved;
+    }
+
+    showUserName() {
+        console.log(`O nome do usuário é ${this.name}`);
+    }
+    showUserRole(canShow: boolean): void {
+        if(canShow) {
+            console.log(`Idade do usuário é: ${this.role}`);
+            return;
+        }
+        console.log("Informação restrita!");
+    }
+}
+
+const zeca = new User("Zéca", "Admin", true);
+
+console.log(zeca);
+
+zeca.showUserName();
+zeca.showUserRole(false);
+
+//interfaces em classes
